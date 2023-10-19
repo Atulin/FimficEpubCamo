cd331fbaf819a91f8b7955826b78d7a22fccc03a
Atulin <11233299+Atulin@users.noreply.github.com>
2023-10-19 22:36:40 +0200

    Everything works

--- FimficEpubCamo/Program.cs
+++ FimficEpubCamo/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
+
+// Get source directory
 Console.WriteLine("Input source directory:");
 var dir = Console.ReadLine() ?? "";
 
+// Get all chapter files in the directory
+var files = Directory.EnumerateFiles(dir, "*.*", SearchOption.AllDirectories).ToImmutableArray();
+var chapters = files.Where(f => f.Contains("chapter-")).ToImmutableArray();
+var others = files.Except(chapters);
+Console.WriteLine($"Found {files.Length} files, {chapters.Length} chapters");
 
-var files = Directory.EnumerateFiles(dir).Where(f => f.Contains("chapter-")).ToImmutableArray();
-Console.WriteLine($"Found {files.Length} files");
-
-
+// Get output directory
 Console.WriteLine("Input out directory:");
 var output = Console.ReadLine() ?? "";
 
-
+// Create it if it doesn't exist
 if (!Directory.Exists(output))
 {
     Console.WriteLine("Directory does not exist, creating...");
@@ -20,27 +25,57 @@ if (!Directory.Exists(output))
     Console.WriteLine($"Created {output}");
 }
 
+// Copy files
+Console.WriteLine("Copying other files");
 
-await Task.WhenAll(files.Select(f => EditFile(dir, output, f)));
+foreach (var directory in Directory.GetDirectories(dir, "*", SearchOption.AllDirectories))
+{
+    Directory.CreateDirectory(directory.Replace(dir, output));
+}
+foreach (var file in others)
+{
+    File.Copy(file, file.Replace(dir, output));
+}
 
-Console.WriteLine("Finished all");
+
+// Asynchronously process chapters
+Console.WriteLine("Processing chapters...");
+await Task.WhenAll(chapters.Select(f => EditFile(dir, output, f)));
+
+
+
+Console.WriteLine("Finished all!");
+
+Process.Start(new ProcessStartInfo
+{
+    Arguments = output,
+    FileName = "explorer.exe"
+});
+
 return;
 
 
 async Task EditFile(string sourceDir, string targetDir, string fileName)
 {
-    Console.WriteLine($"Modifying {fileName}");
+    Console.WriteLine($"\tModifying {fileName}");
     var text = await File.ReadAllTextAsync(fileName);
 
-    var changed = MyRegex().Replace(text, $"""
-                                           "{CleanUrl("$1")}"
-                                           """);
+    var matches = MyRegex().Matches(text);
+    foreach (Match match in matches)
+    {
+        text = text.Replace(match.Value, $"""
+                                          "{match.Groups[1].Value.CleanUrl()}"
+                                          """);
+    }
     
-    await File.WriteAllTextAsync(fileName.Replace(sourceDir, targetDir), changed);
-    Console.WriteLine($"Finished {fileName}");
+    await File.WriteAllTextAsync(fileName.Replace(sourceDir, targetDir), text);
+    Console.WriteLine($"\tFinished {fileName}");
 }
 
-string CleanUrl(string url) => url.Replace("%3A", ":").Replace("%2F", "/");
+internal static class StringExtensions
+{
+    public static string CleanUrl(this string url) => url.Replace("%3A", ":").Replace("%2F", "/");
+}
 
 internal partial class Program
 {
